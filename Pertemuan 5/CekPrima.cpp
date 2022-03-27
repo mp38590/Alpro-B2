85e51050cab9080c9797cdddc741655cf4dbaa40
mp38590 <100133899+mp38590@users.noreply.github.com>
2022-03-27 11:30:03 +0700

    Add files via upload

---
+++ Pertemuan 5/CekPrima.cpp
@@ -0,0 +1,48 @@
+/*Nama File     : CekPrima.c*/
+/*Deskripsi     : Mengecek apakah suatu bilangan integer sembarang N (N>0) merupakan bilangan prima atau bukan*/
+/*Pembuat       : Mutiara Permata Putri - 24060121140154
+/*Tgl Pembuatan : Jumat, 25 03 2022 22:00*/
+
+#include<stdio.h>
+
+int main()
+{
+    //Kamus
+    int N; //Bilangan integer sembarang
+    int i; //Counter
+    int c; //Cek bilangan prima
+
+    //Algoritma
+    printf("Program mengecek bilangan prima atau bukan \n");
+    printf("Masukkan bilangan N : ");
+    scanf("%d", &N);
+
+    if (N <= 0)
+    {
+        printf("N harus bilangan positif");
+    }
+    else
+    {
+        c = 0;
+
+        for (i = 1; i <= N; i++)
+        {
+            if (N % i == 0)
+            {
+                c++;
+            }
+        }
+        if (c == 2)
+        {
+            printf("N merupakan bilangan prima");
+        }
+        else
+        {
+            printf("N bukan merupakan bilangan prima");
+        }
+        {
+            printf("\n\n", N);
+        }
+    }
+    return 0;
+}
